f9c01527e1367b3b6377674f610147e5ce317c48
Andrew <aawferris@gmail.com>
2020-11-11 09:53:00 -0500

    imported react-bootsrap elements for nav

--- cia-cover-story/src/components/shared/Navbar/Navbar.jsx
+++ cia-cover-story/src/components/shared/Navbar/Navbar.jsx
@@ -1,39 +1,41 @@
 import React from 'react';
-import Nav from 'react-bootstrap/Nav'
-import Dropdown from 'react-bootstrap/Dropdown'
-import DropdownButton from 'react-bootstrap/DropdownButton'
+import {Nav, Navbar, NavDropdown, Form, FormControl, Button} from 'react-bootstrap'
 
 import "./Navbar.css"
 
-function Navbar(props) {
+function Navigation(props) {
   return (
     <div id="nav-container">
-      <div >
-        <Nav id="nav-elements" activeKey="/home" onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}>
-          <Nav.Item className="nav-item">
-            <Nav.Link className="nav-link" href="https://www.cia.gov/index.html" target="_blank" rel="noopener noreferrer">Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item className="nav-item">
-            <Nav.Link className="nav-link" href="https://en.wikipedia.org/wiki/Central_Intelligence_Agency" target="_blank" rel="noopener noreferrer" eventKey="link-1">About Us</Nav.Link>
-          </Nav.Item>
-          <Nav.Item className="nav-item">
-            <Nav.Link className="nav-link" href="https://www.google.com/" target="_blank" rel="noopener noreferrer" eventKey="link-2">Intelligence</Nav.Link>
-          </Nav.Item>
-          <Nav.Item className="nav-item">
-            <Nav.Link className="nav-link" href="https://www.addictinggames.com/action/spy" target="_blank" rel="noopener noreferrer" eventKey="link-3" disabled>Training</Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </div>
-      <div id="dropdown-container">
-      <DropdownButton id="dropdown-item-button" title="Language">
-        <Dropdown.ItemText>Language</Dropdown.ItemText>
-        <Dropdown.Item as="/">English</Dropdown.Item>
-        <Dropdown.Item as="/espanol">Español</Dropdown.Item>
-        <Dropdown.Item as="arabic">عربى</Dropdown.Item>
-      </DropdownButton>
-      </div>
+      <Navbar bg="light" expand="lg">
+        {/* <Navbar.Brand id="nav-brand" href="/">CIA Cover Story</Navbar.Brand> */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+            <div id="nav-link-container">
+              <Nav.Link className="nav-link" href="https://www.cia.gov/index.html" target="_blank" rel="noopener noreferrer">Home</Nav.Link>
+              <Nav.Link className="nav-link" href="https://en.wikipedia.org/wiki/Central_Intelligence_Agency" target="_blank" rel="noopener noreferrer" eventKey="link-1">About Us</Nav.Link>
+              <Nav.Link className="nav-link" href="https://www.google.com/" target="_blank" rel="noopener noreferrer" eventKey="link-2">Intelligence</Nav.Link>
+              <Nav.Link className="nav-link" href="https://www.addictinggames.com/action/spy" target="_blank" rel="noopener noreferrer" eventKey="link-3" disabled>Training</Nav.Link>
+                <NavDropdown.Divider />
+            </div>
+            <div id="nav-drop-container">
+            <NavDropdown title="Language" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/">English</NavDropdown.Item>
+              <NavDropdown.Item href="/es">Español</NavDropdown.Item>
+              <NavDropdown.Item href="/ar">عربى</NavDropdown.Item>
+              </NavDropdown>
+            </div>
+          </Nav>
+          <div id="form-container">
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+            </div>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
 
-export default Navbar;
+export default Navigation;
